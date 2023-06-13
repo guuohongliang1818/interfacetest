b7e6c52277ba71b1b50dd5bdb122654078789299
guohongliang <guohongliang@163.com>
2023-06-13 21:43:06 +0800

    接口自动化测试L2阶段文件代理测试

--- interface/l2/test_jsonpath.py
+++ interface/l2/test_jsonpath.py
@@ -1,4 +1,12 @@
 # 姓名：郭宏亮
 # 时间：2023/6/11 18:29
+import jsonpath
+import requests
+
+
 def test_jsonpath():
-    pass
+    r = requests.post("https://httpbin.ceshiren.com/post",
+                      json={"name": [{"age": 18}]})
+
+    print(r.text)
+    print(jsonpath.jsonpath(r.json(), "$..age")[0])
