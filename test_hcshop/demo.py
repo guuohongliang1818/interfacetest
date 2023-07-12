e5b125ee4c71ba8dbc72395609afdaca2db78230
guohongliang <guohongliang@eyou.com>
2023-07-12 15:49:14 +0800

    华测shop获取token接口

--- test_hcshop/demo.py
+++ test_hcshop/demo.py
@@ -10,6 +10,17 @@ data = {
 # 写法1
 res = requests.post(
     url="http://shop-xo.hctestedu.com/index.php?s=/api/user/login&application=app&application_client_type=weixin",
-    json=data)
+    data=data)
 print(res.text)
-print("token：", jsonpath.jsonpath(res.json(), "$..token")[0])
+print("写法一token：", jsonpath.jsonpath(res.json(), "$..token")[0])
+print("================================================================")
+# 写法2
+paramData = {
+    "application": "app",
+    "application_client_type": "weixin"
+}
+res1 = requests.post(url="http://shop-xo.hctestedu.com/index.php?s=/api/user/login", params=paramData, json=data)
+print(res1.text)
+print("写法一token：", jsonpath.jsonpath(res1.json(), "$..token")[0])
+
+print("paramData", paramData)
